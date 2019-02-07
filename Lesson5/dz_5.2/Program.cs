cd07831aa14709cfaf54ce95893c2e4e0a6f66d7
sinandruha <sinandruha@ya.ru>
2019-02-07 18:18:22 +0300

    сделал дз 5.2 без пункта д)

--- Lesson5/dz_5.2/Program.cs
+++ Lesson5/dz_5.2/Program.cs
@@ -17,6 +17,9 @@ using System.Text.RegularExpressions;
 
 namespace dz_5._2
 {
+    /// <summary>
+    /// Класс для обработки текста
+    /// </summary>
     static class Message
     {
         /// <summary>
@@ -62,30 +65,84 @@ namespace dz_5._2
             }
             return result;
         }
-        //public static string RemoveAt(string text, char[] letterEnd)
-        //{
-        //    string[] delim = new string[] { ",", " " };
-        //    string[] parts = text.Split(delim, StringSplitOptions.RemoveEmptyEntries);
-        //    string result = "";
+        /// <summary>
+        /// Удаляет из сообщения все слова, которые заканчиваются на заданные символы.
+        /// </summary>
+        /// <param name="text">Текст сообщения</param>
+        /// <param name="letterEnd">Массив символов</param>
+        /// <returns>Строка слов соотвествующих заданным параметрам</returns>
+        public static string RemoveAt(string text, char[] letterEnd)
+        {
+            string[] delim = new string[] { ",", " " };
+            string[] parts = text.Split(delim, StringSplitOptions.RemoveEmptyEntries);
+            string result = "";
 
-        //    for (int i = 0; i < parts.Length; i++)
-        //    {
-        //        for (int j = 0; j < letterEnd.Length; j++)
-        //        {
-        //            int count = 0;
-        //            if (!(parts[i][parts[i].Length - 1] == letterEnd[j]) && !(count==1))
-        //            {
-        //                count++;
-        //                result += parts[i] + " ";
-        //            }
-        //        }
-        //    }
-        //    return result;
-        //}
+            for (int i = 0; i < parts.Length; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < letterEnd.Length; j++)
+                {
+                    if (!(parts[i][parts[i].Length - 1] == letterEnd[j]))
+                    {
+                        count++;
+                        if(count == letterEnd.Length) result += parts[i] + " ";
+                    }
+                }
+            }
+            return result;
+        }
+        /// <summary>
+        /// Ищет самое длинное слово сообщения(если их несколько, то записывает их через пробел)
+        /// </summary>
+        /// <param name="text">Текст сообщения</param>
+        /// <returns>Строка слов соотвествующих заданным параметрам</returns>
+        public static string WordMaxLength(string text)
+        {
+            string[] delim = new string[] { ",", " " };
+            string[] parts = text.Split(delim, StringSplitOptions.RemoveEmptyEntries);
+            string result = parts[0];
+            string result2 = "";
+            int max = parts[0].Length;
+
+            for(int i = 1; i < parts.Length; i++)
+            {
+                if (parts[i].Length > max)
+                {
+                    max = parts[i].Length;
+                    result = parts[i];
+                }
+            }
+            for(int i = 1; i < parts.Length; i++)
+            {
+                if (parts[i].Length == max)
+                {
+                    result2 += parts[i] + " ";
+                }
+            }
+            return result2;
+        }
+        /// <summary>
+        /// Формирует строку с помощью StringBuilder из самых длинных слов сообщения.
+        /// </summary>
+        /// <param name="text">Текст сообщения</param>
+        /// <returns>Экземпляр класса StringBuilder</returns>
+        public static StringBuilder MaxWordsSB(string text)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append (WordMaxLength(text));
+            return sb;
+        }
+       //// ***Создать метод, который производит частотный анализ текста.В качестве параметра в него передается текст и массив слов,
+       ////в качестве результата метод возвращает сколько раз каждое из слов массива входит в этот текст.Здесь требуется использовать класс Dictionary.
+       //public static int FreqTest(string text, string[] arrword)
+       // {
+       //     Dictionary<string, string> keys = new Dictionary<string,string>();
+       //     keys.
+       // }
     }
     class Program
     {
-        public static void Print(string[] s)
+        public static void PrintStrArr(string[] s)
         {
             for(int i = 0; i < s.Length; i++)
             {
@@ -99,16 +156,24 @@ namespace dz_5._2
             string[] delim = new string[] { ",", " " };
             string[] parts = message.Split(delim, StringSplitOptions.RemoveEmptyEntries);
             char[] sep = {'ь','а','о'}; 
-            Print(parts);
+            PrintStrArr(parts);
 
-            Console.WriteLine(Message.WordFiltr(message, 3));
-            Console.ReadLine();
+            //Console.WriteLine(Message.WordFiltr(message, 3));
+            //Console.ReadLine();
 
-            Console.WriteLine(Message.RemoveAt(message, 'ь'));
-            Console.ReadLine();
+            //Console.WriteLine(Message.RemoveAt(message, 'ь'));
+            //Console.ReadLine();
 
             //Console.WriteLine(Message.RemoveAt(message, sep));
             //Console.ReadLine();
+
+            string message1 = "Яд, мяч, ком, лом, тор, кот, рот, ель";
+            Console.WriteLine(Message.WordMaxLength(message1));
+            Console.ReadLine();
+
+            var maxwords = Message.MaxWordsSB(message1);
+            Console.WriteLine(maxwords);
+            Console.ReadLine();
 
         }
     }
