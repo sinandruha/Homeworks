581a3f0d517171e7b3fa1be1c11f8a6649fcec8b
sinandruha <sinandruha@ya.ru>
2019-02-17 01:23:10 +0300

    add lesson8

---
+++ Lesson8/dz_8.1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Threading;
+
+//Синицын
+//1. С помощью рефлексии выведите все свойства структуры DateTime
+
+namespace dz_8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            object date2 = new DateTime(1999, 10, 31, 11, 46, 54, 999, (DateTimeKind)2);
+
+
+            if (date2.GetType() == typeof(DateTime))
+            {
+                DateTime date1 = (DateTime)date2;
+                Console.WriteLine(date1.Date);
+                Console.WriteLine(date1.Day);
+                Console.WriteLine(date1.DayOfWeek);
+                Console.WriteLine(date1.DayOfYear);
+                Console.WriteLine(date1.Hour);
+                Console.WriteLine(date1.Kind);
+                Console.WriteLine(date1.Millisecond);
+                Console.WriteLine(date1.Minute);
+                Console.WriteLine(date1.Month);
+                Console.WriteLine(date1.Second);
+                Console.WriteLine(date1.Ticks);
+                Console.WriteLine(date1.TimeOfDay);
+                Console.WriteLine(date1.Year);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
